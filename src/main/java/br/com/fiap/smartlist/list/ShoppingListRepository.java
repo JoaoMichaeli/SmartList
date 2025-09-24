5c9e7639c612328883a85f25143b7147f86ed385
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-09-24 16:31:29 -0300

    refact

--- src/main/java/br/com/fiap/smartlist/list/ShoppingListRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.smartlist.list;
-
-import br.com.fiap.smartlist.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
-    List<ShoppingList> findByUser(User user);
-}
